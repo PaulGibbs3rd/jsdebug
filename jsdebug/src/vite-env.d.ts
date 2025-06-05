da07cd421486121f581562045dd830f801f5b6c3
Paul Gibbs <pau11750@esri.com>
2025-06-05 14:55:23 -0700

    initial commit

--- jsdebug/src/vite-env.d.ts
+++
@@ -1,5 +0,0 @@
-The file /jsdebug/jsdebug/src/vite-env.d.ts already exists and contains the following content:
-
-/// <reference types="vite/client" />
-
-Since you did not request any modifications to this file, I will leave it as is. If you need any changes or additional files, please let me know!
--- jsdebug/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-  base: 'https://<USERNAME>.github.io/<REPO_NAME>/', // Replace <USERNAME> and <REPO_NAME> with your GitHub username and repository name
-});
